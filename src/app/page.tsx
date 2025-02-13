839da3f6443c162a5602cf062a70eb911f5e57e9
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-02-13 23:47:28 +0530

    chore: check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import { Button } from "@/components/ui/button";
 import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default async function Home() {
-
   return (
     <div>
-      Hello
-      <Button>Click</Button>
-      <SignOutButton />
+      <div>Landing Page</div>
+      <div className="w-20 bg-red-500 p-2">
+        <SignOutButton />
+      </div>
+      <Link href="/dashboard" className="bg-green-200 p-2">
+        /dashboard
+      </Link>
+      <Link href="/sign-up" className="bg-green-200 p-2">
+        /sign-up
+      </Link>
+      <Link href="/sign-in" className="bg-green-200 p-2">
+        /sign-in
+      </Link>
     </div>
   );
 }
