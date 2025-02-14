096899baf57cafcd28668d7910b8ceb50559ca0d
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-02-15 02:04:49 +0530

    feat: dashboard with project GitHub link and archive button

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,17 +1,42 @@
-"use client"
-import useProject from '@/hooks/use-project'
-import { useUser } from '@clerk/nextjs'
-import React from 'react'
+"use client";
+import useProject from "@/hooks/use-project";
+import { useUser } from "@clerk/nextjs";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Dashboard = (props: Props) => {
-    const {user} = useUser();
+  const { user } = useUser();
 
-   const {project} = useProject()
+  const { project } = useProject();
   return (
-    <div>{project?.name}</div>
-  )
-}
+    <div>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        <div className="w-fit rounded-md bg-primary px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is connected to
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>Archive Button</div>
+      </div>
+      <div className="h-4"></div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">Ask question</div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
