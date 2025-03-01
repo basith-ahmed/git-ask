4bd3d6a14e8df03bc4f8a1907749116e22020109
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-03-02 01:43:59 +0530

    fix: decrement user credits after creating a new project

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import useRefetch from "@/hooks/use-refetch";
 import { api } from "@/trpc/react";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Info } from "lucide-react";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -17,25 +17,37 @@ type FormInput = {
 const CreatePage = () => {
   const { register, handleSubmit, reset } = useForm<FormInput>();
   const createProject = api.project.createProject.useMutation();
+  const checkCredits = api.project.checkCredits.useMutation();
   const refetch = useRefetch();
 
   function onSubmit(data: FormInput) {
-    createProject.mutate({
-      name: data.projectName,
-      githubUrl: data.repoUrl,
-      githubToken: data.githubToken
-    }, {
-        onSuccess: () => {
-            toast.success("Project created successfully")
+    if (!!checkCredits.data) {
+      createProject.mutate(
+        {
+          name: data.projectName,
+          githubUrl: data.repoUrl,
+          githubToken: data.githubToken,
+        },
+        {
+          onSuccess: () => {
+            toast.success("Project created successfully");
             refetch();
             reset();
+          },
+          onError: () => {
+            toast.error("Failed to create the project");
+          },
         },
-        onError: () => {
-            toast.error("Failed to create the project")
-        }
-    });
-    return true;
+      );
+    } else {
+      checkCredits.mutate({
+        githubUrl: data.repoUrl, githubToken: data.githubToken
+      })
+    }
   }
+
+  // frontend validation to check whether the user has enough credits,  but we also need to do backend validation
+  const hasEnoughCredits = checkCredits?.data?.userCredits ? checkCredits.data.fileCount <= checkCredits.data.userCredits : true
 
   return (
     <div className="flex h-full items-center justify-center">
@@ -65,7 +77,19 @@ const CreatePage = () => {
               {...register("githubToken")}
               placeholder="GitHub access token For private repository access (optional)"
             />
-            <Button type="submit" disabled={createProject.isPending}>
+            {!!checkCredits.data && (
+              <>
+              <div className="mt-4 bg-orange-50 px-4 py-2 rounded-md border border-orange-200 text-orange-700">
+                <div className="flex items-center gap-2 ">
+                  <Info  className="size-4"/>
+                  <p className="text-sm">You will be charged <strong>{checkCredits.data?.fileCount}</strong> credits for this repository.</p>
+                </div>
+                <p className="text-sm text-blue-600 ml-6">You have <strong>{checkCredits.data?.userCredits}</strong> credits remaining.</p>
+              </div>
+              </>
+            )}
+            <Button type="submit" disabled={createProject.isPending || checkCredits.isPending || !hasEnoughCredits}>
+              {!!checkCredits.data ? 'Create Project' : 'Check Credits'}
               Create Project <ArrowRight />
             </Button>
           </form>
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { pollCommits } from "@/lib/github";
-import { indexGithubRepo } from "@/lib/github-loader";
+import { checkCredits, indexGithubRepo } from "@/lib/github-loader";
 
 export const projectRouter = createTRPCRouter({
   createProject: protectedProcedure
@@ -13,6 +13,26 @@ export const projectRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
+      const user = await ctx.db.user.findUnique({
+        where: {
+          id: (await ctx.user).userId!,
+        },
+        select: {
+          credits: true,
+        },
+      });
+
+      if (!user) {
+        throw new Error("User not found!");
+      }
+
+      const currentCredits = user.credits || 0;
+      const fileCount = await checkCredits(input.githubUrl, input.githubToken)
+
+      if (fileCount > currentCredits) {
+        throw new Error('Insufficient Credits')
+      }
+
       const project = await ctx.db.project.create({
         data: {
           githubUrl: input.githubUrl,
@@ -27,6 +47,17 @@ export const projectRouter = createTRPCRouter({
 
       await indexGithubRepo(project.id, input.githubUrl, input.githubToken);
       await pollCommits(project.id);
+      await ctx.db.user.update({
+        where: {
+          id: (await ctx.user).userId!
+        },
+        data: {
+
+          credits: {
+            decrement: fileCount
+          }
+        }
+      })
       return project;
     }),
   getProjects: protectedProcedure.query(async ({ ctx }) => {
@@ -115,18 +146,48 @@ export const projectRouter = createTRPCRouter({
           projectId: input.projectId,
         },
         include: {
-          user: true
+          user: true,
         },
       });
     }),
-  getUserCredits: protectedProcedure.query(async ({ctx}) => {
+  getUserCredits: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.user.findUnique({
       where: {
-        id: (await ctx.user).userId!
+        id: (await ctx.user).userId!,
       },
       select: {
-        credits: true
-      }
-    })
-  })
+        credits: true,
+      },
+    });
+  }),
+  getMyCredits: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.user.findUnique({
+      where: {
+        id: (await ctx.user).userId!,
+      },
+      select: {
+        credits: true,
+      },
+    });
+  }),
+  checkCredits: protectedProcedure
+    .input(
+      z.object({
+        githubUrl: z.string(),
+        githubToken: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const fileCount = await checkCredits(input.githubUrl, input.githubToken);
+      const userCredits = await ctx.db.user.findUnique({
+        where: {
+          id: (await ctx.user).userId!,
+        },
+        select: {
+          credits: true,
+        },
+      });
+
+      return { fileCount, userCredits: userCredits?.credits || 0 };
+    }),
 });
