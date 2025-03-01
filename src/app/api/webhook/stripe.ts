a04fd17db2ab47368211f21f61631d9f4ea9c760
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-03-02 00:21:01 +0530

    feat: added stripe checkout

--- src/app/api/webhook/stripe.ts
+++ src/app/api/webhook/stripe.ts
@@ -1,3 +1,4 @@
+import { db } from "@/server/db";
 import { headers } from "next/headers";
 import { NextResponse, NextRequest } from "next/server";
 import Stripe from "stripe";
@@ -28,13 +29,42 @@ export async function POST(request: NextRequest) {
     );
   }
 
+  const session = event.data.object as Stripe.Checkout.Session;
 
-  const session = event.data.object as Stripe.Checkout.Session
+  console.log(event.type);
 
-  console.log(event.type)
+  if (event.type === "checkout.session.completed") {
+    const credits = Number(session.metadata?.["credits"]);
 
-  if (event.type === 'checkout.session.completed') {
-    const credits = Number(session.metadata?.['credits'])
+    const userId = session.client_reference_id;
+    if (!userId || !credits) {
+      return NextResponse.json(
+        { error: "Missing userId or credits" },
+        { status: 400 },
+      );
+    }
+
+    await db.stripeTransaction.create({
+      data: {
+        userId,
+        credits,
+      },
+    });
+    await db.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        credits: {
+          increment: credits,
+        },
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Credits added successfully" },
+      { status: 200 },
+    );
   }
   return NextResponse.json({
     message: "Hello World!",
