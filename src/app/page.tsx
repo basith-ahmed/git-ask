6f6b64d38c065069712b52e9d18113a7c6246a54
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-05-17 19:05:25 +0530

    chore: updated routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,30 @@
+"use client"
+
 import { SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default async function Home() {
-  return (
-    <div>
-      <div>Landing Page</div>
-      <div className="w-20 bg-red-500 p-2">
-        <SignOutButton />
-      </div>
-      <Link href="/dashboard" className="bg-green-200 p-2">
-        /dashboard
-      </Link>
-      <Link href="/sign-up" className="bg-green-200 p-2">
-        /sign-up
-      </Link>
-      <Link href="/sign-in" className="bg-green-200 p-2">
-        /sign-in
-      </Link>
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
+  // return (
+  //   <div>
+  //     <div>Landing Page</div>
+  //     <div className="w-20 bg-red-500 p-2">
+  //       <SignOutButton />
+  //     </div>
+  //     <Link href="/dashboard" className="bg-green-200 p-2">
+  //       /dashboard
+  //     </Link>
+  //     <Link href="/sign-up" className="bg-green-200 p-2">
+  //       /sign-up
+  //     </Link>
+  //     <Link href="/sign-in" className="bg-green-200 p-2">
+  //       /sign-in
+  //     </Link>
+  //   </div>
+  // );
 }
